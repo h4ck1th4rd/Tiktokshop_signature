3a9ba0c345a521feddc00afc26dba2606934ae79
h4ck1th4rd <mzasimple@gmail.com>
2021-03-30 06:50:41 +0300

    Update result.py
    No demo.html file found, test.html is the only available. otherwise it will throw error AttributeError: 'NoneType' object has no attribute 'text'

--- result.py
+++ result.py
@@ -7,7 +7,7 @@ class Signature(object):
     @classmethod
     def get_signature(cls):
         session = HTMLSession()
-        r = session.get('http://127.0.0.1:3000/demo.html', verify=False)
+        r = session.get('http://127.0.0.1:3000/test.html', verify=False)
         r.html.render()
         d = r.html.find('#pid', first=True)
         return d.text
